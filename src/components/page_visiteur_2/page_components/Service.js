75520b6388866fb0c0726cb6937882c0e819f417
Oubida-Mohamed <m.oubida11@gmail.com>
2023-06-03 10:51:02 +0100

    V4

--- src/components/page_visiteur_2/page_components/Service.js
+++ src/components/page_visiteur_2/page_components/Service.js
@@ -5,7 +5,7 @@ const Service =(props) =>{
     const [dore,setdore] = useState(false);
 
     return <div className="w-[250px] h-[300px] bg-white shadow-xl hover:shadow-2xl mx-1 rounded-lg my-3">
-                <svg xmlns="http://www.w3.org/2000/svg" className="absolute w-[25px] h-[25px] ml-[220px] mt-[270px]" onClick={() => { setdore(!dore) }}
+                <svg xmlns="http://www.w3.org/2000/svg" className="absolute w-[25px] h-[25px] ml-[219px] mt-[266px]" onClick={() => { setdore(!dore) }}
                         fill={dore ? "red" : "none"} viewBox="0 0 24 24" stroke={dore ? "red" : "gray"} enableBackground="" >
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
